7e6e882a18bf4683031b74a91eb0b485f8c7b62e
Varun <vjoshi@appcelerator.com>
2012-06-13 12:24:43 -0700

    Formatting

--- Resources/app.js
+++ Resources/app.js
@@ -1,109 +1,109 @@
 var win = Ti.UI.createWindow({
-   backgroundColor: 'white' 
+    backgroundColor : 'white'
 });
 
 win.open();
 
 
+/*
+ * First Step: Require cloudpush module
+ */
 var CloudPush = require('ti.cloudpush');
 CloudPush.debug = true;
 CloudPush.enabled = true;
+
+CloudPush.showTrayNotificationsWhenFocused = true;
+CloudPush.focusAppOnPush = false;
+
 var deviceToken;
 var Cloud = require('ti.cloud');
- 
-CloudPush.retrieveDeviceToken({
-    success: function deviceTokenSuccess(e) {
-        alert('Device Token: ' + e.deviceToken);
-        deviceToken = e.deviceToken
-        loginDefault(deviceToken);
-    },
-    error: function deviceTokenError(e) {
-        alert('Failed to register for push! ' + e.error);
-    }
+
+var registerButton = Ti.UI.createButton({
+    title : 'Register For Push',
+    color : '#000',
+    height : 53,
+    width : 200,
+    top : 100,
 });
- 
-function loginDefault(e){
-    if(!Ti.App.Properties.getString('cloudUser'))
-    {
-        Ti.App.Properties.setString('cloudUser',e)
- 
-        Cloud.Users.create(
-        {
-            username: e,
-            password: 'logmein',
-            password_confirmation: 'logmein'
+
+win.add(registerButton);
+
+/*
+ * Second Step: Retrieve the device token using cloudpush module (only for Android)
+ */
+    
+registerButton.addEventListener('click', function(e) {
+    
+
+    CloudPush.retrieveDeviceToken({
+        success : function deviceTokenSuccess(e) {
+            alert('Device Token: ' + e.deviceToken);
+            deviceToken = e.deviceToken
+            loginDefault(deviceToken);
         },
-        function (e) 
-        {
-            if (e.success)
-            {
-                Cloud.Users.login(
-                {
-                    login: Ti.App.Properties.getString('cloudUser'),
-                    password: 'logmein'
-                }, 
-                function (e) 
-                {
-                    if (e.success) 
-                    {
-                        defaultSubscribe();
-                    } 
-                    else 
-                    {
-                        alert('Error:\\n' +((e.error && e.message) || JSON.stringify(e)));
-                    }
-                });
-            }
-            else 
-            {
-                alert('Error:\\n' +((e.error && e.message) || JSON.stringify(e)));
-            }
-        });
- 
-    }
-    else
-    {
-        Cloud.Users.login(
-         {
-            login: Ti.App.Properties.getString('cloudUser'),
-            password: 'logmein'
-        }, 
-        function (e) 
-        {
-            if (e.success) 
-            {
-                defaultSubscribe();
-            } 
-            else 
-            {
-                alert('Error:\\n' +((e.error && e.message) || JSON.stringify(e)));
-            }
-        });
-    }
- 
+        error : function deviceTokenError(e) {
+            alert('Failed to register for push! ' + e.error);
+        }
+    });
+
+});
+
+
+/*
+ * Third Step: Log in as a user
+ */
+
+
+function loginDefault(e) {
+
+    Cloud.Users.login({
+        login : 'cloudUser',
+        password : 'test123'
+    }, function(e) {
+        if (e.success) {
+            defaultSubscribe();
+        } else {
+            alert('Error:\\n' + ((e.error && e.message) || JSON.stringify(e)));
+        }
+    });
+
 }
- 
-function defaultSubscribe(){
+
+
+/*
+ * Fourth Step: Subscribe a channel
+ */
+
+function defaultSubscribe() {
     Cloud.PushNotifications.subscribe({
-        channel: 'alert',
-        device_token: deviceToken,
-        type: 'android'
-    }, function (e) {
+        channel : 'alert',
+        device_token : deviceToken,
+        type : 'android'
+    }, function(e) {
         if (e.success) {
             alert('Subscribed!');
-        }
-        else {
-            alert('Error:' +((e.error && e.message) || JSON.stringify(e)));
+        } else {
+            alert('Error:' + ((e.error && e.message) || JSON.stringify(e)));
         }
     });
 }
- 
-CloudPush.addEventListener('callback', function (evt) {
+
+
+/*
+ * Fifth Step: Listen for the event callback
+ */
+
+CloudPush.addEventListener('callback', function(evt) {
     alert(evt);
 });
-CloudPush.addEventListener('trayClickLaunchedApp', function (evt) {
+
+
+
+CloudPush.addEventListener('trayClickLaunchedApp', function(evt) {
     Ti.API.info('Tray Click Launched App (app was not running)');
 });
-CloudPush.addEventListener('trayClickFocusedApp', function (evt) {
+
+
+CloudPush.addEventListener('trayClickFocusedApp', function(evt) {
     Ti.API.info('Tray Click Focused App (app was already running)');
 });
